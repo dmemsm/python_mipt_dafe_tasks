aef1f61aaef247c9642675b16a2f725509a03ca6
Mihail Barinov <dmemsmdmemsm@gmail.com>
2026-03-13 23:46:52 +0300

    Homework for lesson 4

--- solutions/sem02/lesson04/task1.py
+++ solutions/sem02/lesson04/task1.py
@@ -2,16 +2,35 @@ import numpy as np
 
 
 def pad_image(image: np.ndarray, pad_size: int) -> np.ndarray:
-    # ваш код
-    return image
+    if pad_size < 1:
+        raise ValueError
+
+    h, w = image.shape[:2]
+    new_h, new_w = h + pad_size * 2, w + pad_size * 2
+    res_form = (new_h, new_w) if image.ndim == 2 else (new_h, new_w, image.shape[2])
+    result = np.zeros(res_form, dtype=image.dtype)
+    result[pad_size : pad_size + h, pad_size : pad_size + w, ...] = image
+    return result
 
 
 def blur_image(
     image: np.ndarray,
     kernel_size: int,
 ) -> np.ndarray:
-    # ваш код
-    return image
+    if kernel_size < 1 or kernel_size % 2 == 0:
+        raise ValueError
+
+    if kernel_size == 1:
+        return image
+
+    padded_image = pad_image(image, kernel_size // 2)
+    h, w = image.shape[:2]
+    windows = [
+        padded_image[i : i + h, j : j + w, ...]
+        for j in range(kernel_size)
+        for i in range(kernel_size)
+    ]
+    return np.mean(windows, axis=0).astype(image.dtype)
 
 
 if __name__ == "__main__":
--- solutions/sem02/lesson04/task2.py
+++ solutions/sem02/lesson04/task2.py
@@ -5,6 +5,23 @@ def get_dominant_color_info(
     image: np.ndarray[np.uint8],
     threshold: int = 5,
 ) -> tuple[np.uint8, float]:
-    # ваш код
+    if threshold < 1:
+        raise ValueError("threshold must be positive")
 
-    return 0, 0
+    pixels = image.flatten()
+    counts = np.zeros(256)
+    for color in range(256):
+        counts[color] = np.sum(pixels == color)
+
+    result_color, result_count = 0, 0
+    for color in range(256):
+        if counts[color] == 0:
+            continue
+        min_with_threshold = max(0, color - (threshold - 1))
+        max_with_threshold = min(255, color + (threshold - 1))
+        count = np.sum(counts[min_with_threshold : max_with_threshold + 1])
+        if count > result_count:
+            result_count = count
+            result_color = color
+
+    return np.uint8(result_color), result_count / pixels.size * 100

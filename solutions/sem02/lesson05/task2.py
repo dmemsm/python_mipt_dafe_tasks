e824447c58330fc55097de08a61647f9abea59f3
Mihail Barinov <dmemsmdmemsm@gmail.com>
2026-03-20 23:33:38 +0300

    Homework for lesson 5

--- solutions/sem02/lesson05/task2.py
+++ solutions/sem02/lesson05/task2.py
@@ -8,4 +8,14 @@ class ShapeMismatchError(Exception):
 def get_projections_components(
     matrix: np.ndarray,
     vector: np.ndarray,
-) -> tuple[np.ndarray | None, np.ndarray | None]: ...
+) -> tuple[np.ndarray | None, np.ndarray | None]:
+    if matrix.shape[0] != matrix.shape[1] or matrix.shape[0] != vector.shape[0]:
+        raise ShapeMismatchError
+
+    if np.linalg.det(matrix) == 0:
+        return None, None
+
+    scalar_result = matrix @ vector
+    lengths = np.sum(matrix**2, axis=1)
+    projections = (scalar_result / lengths)[:, np.newaxis] * matrix
+    return projections, vector - projections
--- solutions/sem02/lesson05/task3.py
+++ solutions/sem02/lesson05/task3.py
@@ -9,4 +9,11 @@ def adaptive_filter(
     Vs: np.ndarray,
     Vj: np.ndarray,
     diag_A: np.ndarray,
-) -> np.ndarray: ...
+) -> np.ndarray:
+    if Vs.shape[0] != Vj.shape[0] or Vj.shape[1] != diag_A.shape[0]:
+        raise ShapeMismatchError
+
+    Vj_h = np.transpose(np.conj(Vj))
+    A = np.diag(diag_A)
+    y = Vs - Vj @ np.linalg.inv(np.eye(Vj.shape[1]) + Vj_h @ Vj @ A) @ (Vj_h @ Vs)
+    return y

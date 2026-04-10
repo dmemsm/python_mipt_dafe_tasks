74bc77cdeb9c2a2a69a9c173f6311450e3c1a783
Mihail Barinov <dmemsmdmemsm@gmail.com>
2026-04-10 23:55:05 +0300

    Homework for lesson 7

--- solutions/sem02/lesson07/task1.py
+++ solutions/sem02/lesson07/task1.py
@@ -8,13 +8,104 @@ class ShapeMismatchError(Exception):
     pass
 
 
+def draw_hist(
+    abscissa: np.ndarray, ordinates: np.ndarray, axis_hor: plt.Axes, axis_vert: plt.Axes
+) -> None:
+    axis_hor.hist(
+        abscissa,
+        bins=50,
+        color="#6093db",
+        density=True,
+        alpha=0.75,
+    )
+
+    axis_vert.hist(
+        ordinates,
+        bins=50,
+        color="#6093db",
+        orientation="horizontal",
+        density=True,
+        alpha=0.75,
+    )
+
+    axis_hor.invert_yaxis()
+    axis_vert.invert_xaxis()
+
+
+def draw_violin(
+    abscissa: np.ndarray, ordinates: np.ndarray, axis_hor: plt.Axes, axis_vert: plt.Axes
+) -> None:
+    axis_hor.violinplot(
+        abscissa,
+        vert=False,
+        showmedians=True,
+    )
+
+    axis_vert.violinplot(
+        ordinates,
+        vert=True,
+        showmedians=True,
+    )
+
+
+def draw_box(
+    abscissa: np.ndarray, ordinates: np.ndarray, axis_hor: plt.Axes, axis_vert: plt.Axes
+) -> None:
+    axis_hor.boxplot(
+        abscissa,
+        vert=False,
+        patch_artist=True,
+        boxprops=dict(facecolor="#d5bd8c"),
+        medianprops=dict(color="k"),
+    )
+    axis_hor.set_yticks([])
+
+    axis_vert.boxplot(
+        ordinates,
+        vert=True,
+        patch_artist=True,
+        boxprops=dict(facecolor="#d5bd8c"),
+        medianprops=dict(color="k"),
+    )
+    axis_vert.set_xticks([])
+
+
 def visualize_diagrams(
     abscissa: np.ndarray,
     ordinates: np.ndarray,
     diagram_type: Any,
 ) -> None:
-    # ваш код
-    pass
+    if abscissa.shape[0] != ordinates.shape[0]:
+        raise ShapeMismatchError
+    if diagram_type not in ["hist", "violin", "box"]:
+        raise ValueError(
+            f"Expected 'hist', 'violin' or 'box' as diagram_type, got '{diagram_type}' instead"
+        )
+
+    plt.style.use("fivethirtyeight")
+    figure = plt.figure(figsize=(8, 8))
+    grid = plt.GridSpec(4, 4, wspace=space, hspace=space)
+
+    axis_scatter = figure.add_subplot(grid[:-1, 1:])
+    axis_vert = figure.add_subplot(
+        grid[:-1, 0],
+        sharey=axis_scatter,
+    )
+    axis_hor = figure.add_subplot(
+        grid[-1, 1:],
+        sharex=axis_scatter,
+    )
+
+    axis_scatter.scatter(abscissa, ordinates, color="#7655ea", alpha=0.5)
+
+    if diagram_type == "hist":
+        draw_hist(abscissa, ordinates, axis_hor, axis_vert)
+    elif diagram_type == "violin":
+        draw_violin(abscissa, ordinates, axis_hor, axis_vert)
+    elif diagram_type == "box":
+        draw_box(abscissa, ordinates, axis_hor, axis_vert)
+
+    plt.show()
 
 
 if __name__ == "__main__":
--- solutions/sem02/lesson07/task2.py
+++ solutions/sem02/lesson07/task2.py
@@ -1,1 +1,51 @@
-# ваш код (используйте функции или классы для решения данной задачи)
+import json
+import os
+import typing
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def read_data(file: str) -> typing.Tuple[np.ndarray, np.ndarray]:
+    with open(file, "r") as f:
+        data = json.load(f)
+    return np.array(data["before"]), np.array(data["after"])
+
+
+def process_data(before: np.ndarray, after: np.ndarray) -> typing.Tuple[np.ndarray, np.ndarray]:
+    _, counts_before = np.unique(before, return_counts=True)
+    _, counts_after = np.unique(after, return_counts=True)
+    return counts_before, counts_after
+
+
+def visualize(counts_before: np.ndarray, counts_after: np.ndarray) -> None:
+    plt.style.use("fivethirtyeight")
+    species = ("I", "II", "III", "IV")
+    counts = {
+        "Before": counts_before,
+        "After": counts_after,
+    }
+
+    x = np.arange(len(species))
+    width = 0.4
+    multiplier = 0
+    figure, axes = plt.subplots(figsize=(16, 9), layout="constrained")
+    axes: plt.Axes
+
+    for attribute, measurement in counts.items():
+        offset = width * multiplier
+        axes.bar(x + offset, measurement, width, label=attribute)
+        multiplier += 1
+
+    axes.set_ylabel("Amount of people")
+    axes.set_title("Mitral disease stages")
+    axes.set_xticks(x + width / 2, species)
+    axes.legend(loc="upper right", ncols=3)
+
+    plt.show()
+
+
+if __name__ == "__main__":
+    before, after = read_data(os.path.join(os.path.dirname(__file__), "data", "medic_data.json"))
+    before_counts, after_counts = process_data(before, after)
+    visualize(before_counts, after_counts)

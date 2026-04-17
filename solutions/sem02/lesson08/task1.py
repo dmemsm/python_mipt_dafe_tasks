1c989797258e93c9aabcc563585c824b6f99923b
Mihail Barinov <dmemsmdmemsm@gmail.com>
2026-04-17 23:57:25 +0300

    Homework for lesson 8

--- solutions/sem02/lesson08/task1.py
+++ solutions/sem02/lesson08/task1.py
@@ -7,6 +7,29 @@ from IPython.display import HTML
 from matplotlib.animation import FuncAnimation
 
 
+def update_frame(
+        frame_id: int,
+        *,
+        line: plt.Line2D,
+        axis: plt.Axes,
+        modulation,
+        fc: float,
+        plot_duration: float,
+        time_step: float,
+        animation_step: float,
+) -> tuple[plt.Line2D]:
+    time_start = frame_id * animation_step
+    time_end = time_start + plot_duration
+    times = np.arange(time_start, time_end, time_step)
+
+    modulated_times = modulation(times) if modulation is not None else np.ones_like(times)
+    ordinates = modulated_times * np.sin(2 * np.pi * fc * times)
+
+    line.set_data(times, ordinates)
+    axis.set_xlim(time_start, time_end)
+    return line,
+
+
 def create_modulation_animation(
     modulation, 
     fc, 
@@ -16,8 +39,33 @@ def create_modulation_animation(
     animation_step=0.01,
     save_path=""
 ) -> FuncAnimation:
-    # ваш код
-    return FuncAnimation()
+    figure, axis = plt.subplots(figsize=(16, 9))
+    axis.set_ylim(-2, 2)
+    axis.set_xlabel("Время (с)")
+    axis.set_ylabel("Амплитуда")
+
+    line, *_ = axis.plot([], [], c="red", lw=2)
+
+    animation = FuncAnimation(
+        figure,
+        partial(
+            update_frame,
+            line=line,
+            axis=axis,
+            modulation=modulation,
+            fc=fc,
+            plot_duration=plot_duration,
+            time_step=time_step,
+            animation_step=animation_step
+        ),
+        frames=num_frames,
+        interval=50,
+        blit=True,
+    )
+
+    if save_path:
+        animation.save(save_path, writer='pillow')
+    return animation
 
 
 if __name__ == "__main__":
